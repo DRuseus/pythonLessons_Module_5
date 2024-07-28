e34f9f36e35004537baab52f06f6f4b16c11071f
Druseus <79805191+DRuseus@users.noreply.github.com>
2024-07-29 00:42:51 +0300

    -Добавлено ещё немного украшательств

--- Module_5_1/module_5_1.py
+++ Module_5_1/module_5_1.py
@@ -2,18 +2,21 @@ class House:
     def __init__(self, name, num_of_floor):
         self.name = name
         while True:
-            if 1 > num_of_floor or num_of_floor > 999:
-                print('\033[31mТакой этажности быть не может!!!\033[0m')
-                num_of_floor = int(input('Введите нужную этажность: '))
+            if 1 > num_of_floor or num_of_floor > 100:
+                print('\033[33mГде вы такой дом нашли? Давайте будем реалистами)))\033[0m')
+                num_of_floor = int(input('Введите нужную этажность (\033[31mНЕ БОЛЬШЕ 100 ЭТАЖЕЙ\033[0m): '))
                 continue
             else:
                 self.num_of_flor = int(num_of_floor)
                 break
 
+
+    # Функция для поиска этажа
     def go_to(self, new_floor):
         draw_list = []
         pointer = str(' ')
 
+        # Логика для отрисовки указателя в консоли
         for p in range(1, new_floor + 1):
             if 10 > p == new_floor:
                 pointer += str('^ - Искомый этаж')
@@ -29,9 +32,11 @@ class House:
             elif 10 <= p < 100:
                 pointer += str('    ')
 
+        # Логика для отрисовки списка этажей
         for floor in range(1, new_floor + 1):
             draw_list.append(floor)
 
+        # Проверка на поиск несуществующего этажа
         if new_floor > self.num_of_flor:
             print(
                 f'\033[31mТакого этажа не существует!!! \nВы не можете подняться на этаж \033[34m{new_floor} \033[31mпри этажности \033[33m{self.num_of_flor}')
@@ -41,5 +46,5 @@ class House:
         return new_floor
 
 
-elb = House('Elbrus', 30)
-elb.go_to(9)
+elb = House('Elbrus', 101)
+elb.go_to(10)
