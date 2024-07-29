51ab67e8a2274fbba9dd8b8a000a703a8a55728b
Druseus <79805191+DRuseus@users.noreply.github.com>
2024-07-29 15:44:26 +0300

    Сделано практическое задание по условию урокуа "Специальные методы классов"

--- Module_5_2/module_5_2.py
+++ Module_5_2/module_5_2.py
@@ -1,5 +1,5 @@
 class House:
-    def __init__(self, name, abs_floor):
+    def __init__(self, name='НЕТ ДАННЫХ', abs_floor=1):
         self.name = name
         self.abs_floor = abs_floor
 
@@ -10,13 +10,19 @@ class House:
         return self.abs_floor
 
 
-h1 = House('ЖК Эльбрус', 10)
+h1 = House('ЖК Эльбрус', 50)
 h2 = House('ЖК Акация', 20)
+h3 = House('Домик в деревне')
+h4 = House(abs_floor=5)
 
 # __str__
 print(h1)
 print(h2)
+print(h3)
+print(h4)
 
 # __len__
 print(len(h1))
 print(len(h2))
+print(len(h3))
+print(len(h4))
